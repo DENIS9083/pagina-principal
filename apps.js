024d195323f70aad8558770d5936b5e4b4091abe
DENIS9083 <melokeehl999@gmail.com>
2024-09-09 17:33:09 -0300

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const path = require ('path')
+
+const app = express();
+const port = 3000;
+
+app.use (express.static (path.join(__dirname,'Public')))
+
+app.get ('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'Public' ,'index.html'));
+});
+app.get ('/sobre',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Public','sobre.html'));
+});
+
+app.listen (port, ()=>{
+    console.log(`Servidor Rodando na em http://127.0.0.1:${port}`);
+});
